95253156391daaf325b86b6a871b566faf4af52f
Duarte6467 <naomexasmano@gmail.com>
2023-01-31 10:49:16 +0000

    Update some code on main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pandas as pd
 import plotly.express as px
 import googlefinance as finance
 from yahooquery import Ticker
+import openpyxl
 
+# Display all collumns
 pd.set_option("display.max_columns", None)
 
 # Companies listed in the Financial Times Stock Exchange 100 and in the Financial Times Stock Exchange 250
@@ -27,17 +29,36 @@ all_symbols = " ".join(all_symbols)
 print(all_symbols)
 
 
-# The loop below is extremely inneficient, other solutions may be more viable!!!!!
-#for a in all_simbols:
- #    data_spef = yf.Ticker(a)
-  #   print(data_spef.info)
-
 
 # There is a way that is more efficient /////// https://stackoverflow.com/questions/71161902/get-info-on-multiple-stock-tickers-quickly-using-yfinance
 # The code below may not be entirely accurate. See website provided above
 
-# Download all info
-my_info = yf.download(all_symbols, start="2022-06-01", end="2023-01-01", group_by="ticker")
+#--------------------------------------------------------------------------------------------------------
+#   This section was made by using the yfinance module ( I believe it is not necessary)
+#   Download all info
 
 
-print(my_info.info)
+#my_info = yf.download(all_symbols, start="2022-06-01", end="2023-01-01", group_by="ticker")
+
+
+# print(my_info.info)
+
+#--------------------------------------------------------------------------------------------------------
+
+
+print("Main Method",100 * "-")
+
+# Select all symbols of the top 30 companies listed in FTSE100
+top30 = Ticker(all_symbols)
+
+
+# Retrieve all financial data from the top 30 companies listed in the FTSE100
+financial_data = top30.all_financial_data()
+
+print(financial_data)
+
+
+# Save all the information to an Excel format
+dataset = financial_data.to_excel("output.xlsx")
+
+
