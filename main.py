916b995a50c57102aedd2b450b403d84307ec42f
Duarte6467 <naomexasmano@gmail.com>
2023-02-02 13:17:35 +0000

    Updated some code on main.py I think that we are going to use yfinance module afterall, because it is way more reliable and far easier to use, although it is slower. Nevertheless, I think it is better. Updated some code  and added some code since line 54

--- main.py
+++ main.py
@@ -8,11 +8,8 @@ import plotly.express as px
 import googlefinance as finance
 from yahooquery import Ticker
 import openpyxl
-
-# Display all columns
 pd.set_option("display.max_columns", None)
-
-
+# Display all columns
 
 # Read the CSV File / contains the top 30 companies listed in the FTSE100 Index
 FTSE = pd.read_csv("EPIC.csv")
@@ -20,51 +17,33 @@ FTSE = pd.read_csv("EPIC.csv")
 # Share Codes
 symbols = FTSE["Symbol"]
 all_symbols = symbols.tolist()
-all_symbols1 = " ".join(all_symbols)
-
-#Reference This
-# https://stackoverflow.com/questions/71161902/get-info-on-multiple-stock-tickers-quickly-using-yfinance
-
-
-# There is a way that is more efficient /////// https://stackoverflow.com/questions/71161902/get-info-on-multiple-stock-tickers-quickly-using-yfinance
-# The code below may not be entirely accurate. See website provided above
-
-
-print("Main Method",100 * "-")
-
-# Select all symbols of the top 30 companies listed in FTSE100
-top30 = Ticker(all_symbols, progress=True)
-
-
-# Retrieve all financial data from the top 30 companies listed in the FTSE100
-financial_data = top30.all_financial_data()
-
-print(financial_data)
-print("-"*100)
-
-
-
-
-# Save all the information to an Excel format
-dataset = financial_data.to_excel("output.xlsx")
-
-
 
 
 # Yfinance module to get all the information that we need / May be the most efficient way to get results, although slower
-# Create empty Dataframe
-info_we_need = pd.DataFrame()
 
-for symbol in all_symbols:
-    all_info = yf.Ticker(symbol).info
-    #Add more parameters here, such as low, high , daily , close P/E ratio and EPS, sentiment analysis, etc
-    sector = all_info.get("sector")   # Sector Parameter
-    info_we_need = info_we_need.append({"Symbol": symbol,"Sector": sector}, ignore_index= True)
+data = yf.download(all_symbols, period="5y", interval="1d", threads= True, group_by= "ticker")
 
-print(info_we_need)
+
+print(data)
+print(type(data))
 
 
 
+empty = pd.DataFrame()
+
+for ticker in all_symbols:
+    ind = yf.Ticker(ticker)
+    sector = ind.info["sector"]
 
 
+
+
+print(sector)
+
+
+
+
+
+
+# This is the part where thhe Machine Learning Tecniques ( Logistic Regresion and whatnot) will do its magic
 
