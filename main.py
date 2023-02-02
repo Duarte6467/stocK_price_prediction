4cd019ee6270b3b6ebeda74e88538317d5a08986
Duarte6467 <naomexasmano@gmail.com>
2023-02-02 14:21:04 +0000

    Updated a lot of code. Now I just need to concatenate the intiial data(the one with low high, open close, etc) with the other parameters that we want to use (Sector, Forward EPS, etc).
    The Forward EPS is actually a very good predictor. Take a look!

--- main.py
+++ main.py
@@ -9,38 +9,38 @@ import googlefinance as finance
 from yahooquery import Ticker
 import openpyxl
 pd.set_option("display.max_columns", None)
-# Display all columns
 
 # Read the CSV File / contains the top 30 companies listed in the FTSE100 Index
 FTSE = pd.read_csv("EPIC.csv")
 
-# Share Codes
+#  Symbol Codes listed in the FTSE100 index / TOP30
 symbols = FTSE["Symbol"]
+
+# Convert format to List
 all_symbols = symbols.tolist()
 
-
 # Yfinance module to get all the information that we need / May be the most efficient way to get results, although slower
-
 data = yf.download(all_symbols, period="5y", interval="1d", threads= True, group_by= "ticker")
 
+# Check Summary Statistics for each Symbol
+summary_statistics = data.describe()
+print(summary_statistics)
+
+# Reference this
+# https://stackoverflow.com/questions/69117454/from-yfinance-to-manipulate-dataframe-with-pandas
+data = data.stack(0).reset_index().rename(columns= {"level_1":"Symbol"})
 
 print(data)
-print(type(data))
 
-
+ # Get the sector for each symbol
 sector_info = pd.DataFrame()
 
 for ticker in all_symbols:
-    ind = yf.Ticker(ticker)
-    sector = ind["sector"]
-    sector_info.append({"Symbol": ticker, "Sector": sector}, ignore_index= True)
-
-
+    inf = yf.Ticker(ticker).info
+    sector = inf.get("sector")
+    sector_info = sector_info.append({"Symbol": ticker, "Sector": sector}, ignore_index= True)
 
 print(sector_info)
-
-
-
 
 
 # This is the part where thhe Machine Learning Tecniques ( Logistic Regresion and whatnot) will do its magic
