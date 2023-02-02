a70ca7961b6009a8f6f3e7597e7f66ab15285fb7
Duarte6467 <naomexasmano@gmail.com>
2023-02-02 17:37:22 +0000

    Updated a lot of code. Just need to add more variables (easy part)
    The Forward EPS is actually a very good predictor. Take a look!

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 import random as rd
+import warnings
+warnings.filterwarnings("ignore")   # Remove deprecated warnings
 import pandas as pd
 import plotly.express as px
 import googlefinance as finance
 from yahooquery import Ticker
 import openpyxl
+import ray
+
 pd.set_option("display.max_columns", None)
 
 # Read the CSV File / contains the top 30 companies listed in the FTSE100 Index
@@ -23,8 +27,8 @@ all_symbols = symbols.tolist()
 data = yf.download(all_symbols, period="5y", interval="1d", threads= True, group_by= "ticker")
 
 # Check Summary Statistics for each Symbol
-summary_statistics = data.describe()
-print(summary_statistics)
+#summary_statistics = data.describe()
+#print(summary_statistics)
 
 # Reference this
 # https://stackoverflow.com/questions/69117454/from-yfinance-to-manipulate-dataframe-with-pandas
@@ -35,13 +39,24 @@ print(data)
  # Get the sector for each symbol
 sector_info = pd.DataFrame()
 
+# Experimental Feature using Ray module
+#https://stackoverflow.com/questions/73123556/downloading-yahoo-finance-data-much-faster-than-using-just-a-for-loop
+
+
+
+
+
+# This part takes ages to run / need to make this efficient
 for ticker in all_symbols:
     inf = yf.Ticker(ticker).info
-    sector = inf.get("sector")
+    sector = inf.info["sector"]
     sector_info = sector_info.append({"Symbol": ticker, "Sector": sector}, ignore_index= True)
 
 print(sector_info)
 
 
-# This is the part where thhe Machine Learning Tecniques ( Logistic Regresion and whatnot) will do its magic
+# Merge the Datasets with a common key (variable--- "Symbol") / Can merge more than 2 DataFrames
+data = pd.merge(data,sector_info, how="inner")
+print(data)
 
+# This is the part where thhe Machine Learning Tecniques ( Logistic Regresion and whatnot) will do its magic
