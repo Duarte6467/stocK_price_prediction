b609d6a5e2c7b417a0ecfeb9f56cffc730d439af
Duarte6467 <naomexasmano@gmail.com>
2023-02-11 13:53:16 +0000

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +13,7 @@ from yahooquery import Ticker
 import openpyxl
 import pandasdmx as pdmx
 import sklearn as sk
+import os
 
 
 pd.set_option("display.max_columns", None)
@@ -40,21 +43,23 @@ print(data)
 #------------------------------------------------------------------------------------------------------------------
 
  # Get the sector for each symbol
-sector_info = pd.DataFrame()
+#sector_info = pd.DataFrame()
 
 # This part takes ages to run / need to make this efficient
-for ticker in all_symbols:
-    inf = yf.Ticker(ticker).info
-    sector = inf.get("sector")
-    sector_info = sector_info.append({"Symbol": ticker, "Sector": sector}, ignore_index= True)
+#for ticker in all_symbols:
+#    inf = yf.Ticker(ticker).info
+#    sector = inf.get("sector")
+#    sector_info = sector_info.append({"Symbol": ticker, "Sector": sector}, ignore_index= True)
 
+#print(sector_info)
+
+#sector_info_csv = sector_info.to_csv("sector_info.csv")
+
+sector_info = pd.read_csv("sector_info.csv")
+
+sector_info = sector_info[["Symbol", "Sector"]]
 print(sector_info)
 
-sector_info_csv = sector_info.to_csv("sector_info.csv")
-
-
-# Reference This
-#https://aeturrell.github.io/coding-for-economists/data-extraction.html
 # Merge the Datasets with a common key (variable--- "Symbol") / Can merge more than 2 DataFrames
 final_dataset = pd.merge(data,sector_info, how="inner")
 print(final_dataset)
@@ -66,11 +71,62 @@ grouped_by = final_dataset.groupby(["Sector","Date"]).mean()
 print(grouped_by)
 
 
+# Plot Graphs to see if there are trends in each sector
+print(sector_info.value_counts("Sector"))
+# Use the final Dataset
+# Make a loop:
 
-plt.figure()
-plt.plot(grouped_by["Date"], grouped_by["Adj Close"])
-plt.grid()
+final_dataset = final_dataset.groupby("Sector")
+
+sector_names  = final_dataset.groups.keys()
+
+# Run only once to decrease completion time
+for sector in sector_names:
+    # Group each sector into one dataset
+    each_sector = final_dataset.get_group(sector)
+
+    # Separate each argument for clearer interpretation
+    name_of_csv = f"{sector}.csv"
+    filepath = os.path.join("Sectors_csv", name_of_csv)
+    each_sector.to_csv(filepath, index = False)
+
+
+
+
+
+
+
+
+
+# Fetch a list of the files included in the directory
+files = [f for f in os.listdir("Sectors_csv") if f.endswith(".csv")]
+# Plot each graph with each correspondent CSV file
+fig , ax = plt.subplots(nrows=1 , ncols= len(files), figsize =(15,5))
+
+# Plot adjusted close of every company ( each chart is separated between its sector) against time
+for i , file in enumerate(files):
+    filepath = os.path.join("Sectors_csv", file)
+    print(filepath)
+    df = pd.read_csv(r"C:\Users\naome\OneDrive\Ambiente de Trabalho\stocK_price_prediction\Sectors_csv")
+    for stock_symbol , group in df.groupby("Symbol"):
+        ax[i].plot(group["Date"], group["Adj Close"], label = stock_symbol)
+        ax[i].set_title(file)
+
+
+plt.tight_layout()
 plt.show()
+# reference this
+
+
+
+
+
+
+
+
+
+
+
 
 # Technical Indicators that will be used
 # Moving Average
@@ -82,14 +138,13 @@ Close = Close.iloc[-1]
 print(Close)
 
 
+
+# Just an example to see if it works!!! We don't need this, but yfinance is not working------10/02/2023
+
 # Momentum added to dataset
-# Just an example to see if it works!!! We dont need this, but yfinance is not working------10/02/2023
-data = data.sort_values(by= ["Symbol", "Date"], ascending= True)
-
-
 data['momentum'] = (data['Adj Close'] - data['Adj Close'].shift(1))  # Momentum of 5 days Calculated
 
-print(data)
+
 
 
 
@@ -110,3 +165,5 @@ print(data)
     #len(T)
     #for i in len(T):
      #   Close.
+
+
